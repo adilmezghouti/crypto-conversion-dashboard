efe237174b1f0ece9bd41d18a8f709006410a7d4
Adil Mezghouti <adilmezghouti@gmail.com>
2022-12-08 19:05:00 -0500

    Added error boundary
    Also updated the logic associated with the convert button

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,16 @@ import styles from '../styles/Home.module.css'
 import {ChangeEventHandler, useCallback, useEffect, useState} from "react"
 import {fetchCurrencies, fetchMetadata, fetchPrice} from './utils'
 import {CurrencyInfo} from "./types";
+import ErrorBoundary from "../components/error-boundary";
 
 
-const Countdown = ({value, callback} : {value: number, callback: () => void}) => {
+const Countdown = ({value, callback}: { value: number, callback: () => void }) => {
   const [counter, setCounter] = useState(value)
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCounter(counter => {
-        if (counter === 0){
+        if (counter === 0) {
           callback()
           return value
         } else {
@@ -43,12 +44,12 @@ export default function Home() {
     setLoading(false)
   }, [from])
 
-  const handleChange:ChangeEventHandler<HTMLSelectElement> = (event) => {
+  const handleChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
     setFrom(event.target.value)
   }
 
-  const handleConvertClick = () => {
-    fetchCurrencies().then(setCurrencies)
+  const handleConvertClick = async () => {
+    await refreshInfo()
   }
 
   useEffect(() => {
@@ -67,22 +68,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Conversion Dashboard
-        </h1>
-        <div className={styles.grid}>
+      <ErrorBoundary>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Conversion Dashboard
+          </h1>
+          <div className={styles.grid}>
           <span className={styles.logo}>
             <img src={logoUrl} alt="Crypto Currency Logo" width={30} height={30}/>
           </span>
-          <select className={styles.select} value={from} onChange={handleChange}>
-            {currencies?.map(({id, symbol}) => (<option key={id} value={symbol}>{symbol}</option>))}
-          </select>
-          <div className={styles.fiat}>{`$${Number(fiat).toFixed(2)}`}</div>
-        </div>
-        {loading ? <div className={styles.loading}>Loading the new rate...</div> : <Countdown value={10} callback={refreshInfo} />}
-        <button className={styles.button} onClick={handleConvertClick}>CONVERT</button>
-      </main>
+            <select className={styles.select} value={from} onChange={handleChange}>
+              {currencies?.map(({id, symbol}) => (<option key={id} value={symbol}>{symbol}</option>))}
+            </select>
+            <div className={styles.fiat}>{`$${Number(fiat).toFixed(2)}`}</div>
+          </div>
+          {loading ? <div className={styles.loading}>Loading the new rate...</div> :
+            <Countdown value={10} callback={refreshInfo}/>}
+          <button className={styles.button} onClick={handleConvertClick}>{loading ? 'CONVERTING...' : 'CONVERT'}</button>
+        </main>
+      </ErrorBoundary>
     </div>
   )
 }
